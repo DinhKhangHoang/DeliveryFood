8e2c198d70a0d2c26125cf8cb3bc96230f2a4df7
ducquynh0106 <48541505+ducquynh0106@users.noreply.github.com>
2019-05-03 21:39:05 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export default data = [
+    {
+        id: 1,
+        Restaurant: 'KFC',
+        Address: '123 Le Van Viet',
+        avatar: './Media/icon/KFC.png',
+    },
+    {
+        id: 2,
+        Restaurant: 'King BBQ',
+        Address: '123 Le Van Viet',
+        avatar: './Media/icon/KFC.png',
+    },
+    {
+        id: 3,
+        Restaurant: 'KFC',
+        Address: '123 Vo Van Ngan',
+        avatar:'KFC.png',
+    },
+    {
+        id: 4,
+        Restaurant: 'Kichi Kichi',
+        Address: '123 Le Van Viet',
+        avatar: './Media/icon/KFC.png',
+    },
+    {
+        id: 5,
+        Restaurant: 'Nam Anh Em',
+        Address: '123 Le Van Viet',
+        avatar: './Media/icon/KFC.png',
+    },
+]
