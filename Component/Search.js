d4245d9be604b8a31205f598187995bf110d0dae
ducquynh0106 <48541505+ducquynh0106@users.noreply.github.com>
2019-05-03 21:38:26 +0700

    fix search

--- Component/Search.js
+++ Component/Search.js
@@ -1,37 +1,103 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { Text,StyleSheet,View,ListView,TouchableHighlight,Dimensions,Image,Animated,TextInput, Alert } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { StackNavigator } from 'react-navigation';
-import resultsearch from './resultsearch'
+import data from '../data'
+import RestaurantInfor from './restaurantInfor'
 
-class Searchpage extends Component {
-	static navigationOptions = {
-									header: null
-					};
-	render() {
-		return (
-			<View>
-				<View style={styles.containImage}>
-					<Image
-						style={styles.imgLogo}
-						resizeMode={'contain'}
-						source={require('../Media/icon/KFC.png')}
-					/>
-					<View style={styles.containtext}>
-						<Text style={styles.textMessage}>KFC</Text>
-						<Text style={styles.textMessage}>123 Vo Van Ngan</Text>
-					</View>
-					<Button
-						title="Menu"
-						onPress={() => this.props.navigation.navigate('resultsearch')}
-					color="blue"
-	/>
-				</View>
-			</View>
-		);
-	}
+const {width, height} = Dimensions.get('window')
+
+ class Searchpage extends Component {
+   	static navigationOptions = {	header: null	};
+    constructor(props){
+        super(props)
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+        this.state = {
+            isLoaded: false,
+            dataSource: ds.cloneWithRows(data),
+            rotateY: new Animated.Value(0),
+            translateX: new Animated.Value(width),
+            menuAnimation: new Animated.Value(0),
+            text: ''
+        }
+    }
+    renderRow(rowData){
+        return (
+            <TouchableHighlight onPress={() => this.props.navigation.navigate('RestaurantInfor')}>
+            <View style={styles.containerCell}>
+                    <View style={styles.footerContainer}>
+                        <View
+                            style={styles.imageUser}
+                        >
+                            <Image
+                                style={styles.imageAvatar}
+                                source={{uri: rowData.avatar}}
+                            />
+                        </View>
+                        <View style={styles.footerTextContainer}>
+                            <Text style={styles.text}>{rowData.Restaurant}</Text>
+                            <Text style={[styles.text, styles.textTitle]}>{rowData.Address}</Text>
+                        </View>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+    filterSearch(text){
+        const newData = data.filter(function(item){
+            const itemData = item.Restaurant.toUpperCase()
+            const textData = text.toUpperCase()
+            return itemData.indexOf(textData) > -1
+        })
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(newData),
+            text: text
+        })
+    }
+    render(){
+        return (
+            <View style={styles.container}>
+               <Animated.View
+                    style={[styles.content, {
+                        width: width,
+                        backgroundColor: 'white',
+                        flex: 1,
+                        transform: [
+                            {
+                                perspective: 450
+                            },
+                            {
+                                translateX: this.state.translateX.interpolate({
+                                    inputRange: [0, width],
+                                    outputRange: [width, 0]
+                                })
+                            },
+                            {
+                                rotateY: this.state.rotateY.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: ['0deg', '-10deg']
+                                })
+                            }
+                        ]
+                    }]}
+                >
+
+                    <TextInput
+                        style={styles.textInput}
+                        onChangeText={(text) => this.filterSearch(text)}
+                        value={this.state.text}
+                    />
+                    <ListView
+                        enableEmptySections={true}
+                        style={styles.listContainer}
+                        renderRow={this.renderRow.bind(this)}
+                        dataSource={this.state.dataSource}
+                    />
+                </Animated.View>
+            </View>
+        )
+    }
 }
-
 export default class Search extends Component
 {
   constructor(props)
@@ -43,7 +109,7 @@ export default class Search extends Component
   {
 	  const Nav = createAppContainer(createStackNavigator({
 		  Search: { screen: Searchpage },
-		  resultsearch: { screen: resultsearch },
+		  RestaurantInfor: { screen: RestaurantInfor },
 	  },
 		  {
 			  initialRouteName: "Search"
@@ -53,25 +119,42 @@ export default class Search extends Component
   }
 }
 
+
 const styles = StyleSheet.create({
-	textMessage: {
-		marginTop: 16,
-		fontSize: 20,
-
-	},
-	containImage: {
-		marginTop: 16,
-		flexDirection: 'row',
-		justifyContent: 'space-between'
-
-	},
-	imgLogo: {
-		width: 100,
-		height: 100,
-
-	},
-	containtext: {
-		flexDirection: 'column',
-		justifyContent: 'flex-start'
-	},
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    textInput: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#cecece',
+        marginBottom: 10,
+        marginHorizontal: 10
+    },
+    content: {
+        zIndex: 1
+    },
+    footerContainer: {
+       flexDirection: 'row',
+       paddingHorizontal: 10,
+       paddingVertical: 10,
+       backgroundColor: 'green'
+    },
+    imageAvatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 5
+    },
+    text: {
+        fontSize:20,
+        color: '#fff'
+    },
+    containerCell: {
+        marginBottom: 10
+    },
+    textTitle: {
+        fontSize: 20
+    },
+})
